9453800b36839a1085c11e158b735d1167a2d734
MichiTheGenius <litzinger04@gmail.com>
2021-11-06 15:55:54 +0100

    added every method to check whether placement is valid or not

--- Solver.py
+++ Solver.py
@@ -1,7 +1,6 @@
 
-
 board = [
-[0,0,0,0,0,0,0,0,0],
+[1,0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,0,0,0],
@@ -21,5 +20,31 @@ def print_board():
                 print("|",end="")
             print(board[row][column], end="")
         print()
+
+def is_number_in_row(number,row):
+    for column in range(9):
+        if board[row][column] == number:
+            return True
+    return False
+
+def is_number_in_column(number,column):
+    for row in range(9):
+        if board[row][column] == number:
+            return True
+    return False
+
+def is_number_in_box(number,row,column):
+    box_row = row - row % 3
+    box_column = column - column % 3
+    for i in range(box_row,box_row+3):
+        for j in range(box_column,box_column+3):
+            if board[i][j] == number:
+                return True
+    return False
+
+def is_valid_place(number,row,column):
+    return not is_number_in_row(number,row) and not is_number_in_column(number,column) and not is_number_in_box(number,row,column)
+
+
 
 print_board()
