5085e49f05b06e0c2bb1a2b8f1f54236f33d144e
MichiTheGenius <litzinger04@gmail.com>
2021-11-06 16:01:08 +0100

    added recursive solve method

--- Solver.py
+++ Solver.py
@@ -45,6 +45,21 @@ def is_number_in_box(number,row,column):
 def is_valid_place(number,row,column):
     return not is_number_in_row(number,row) and not is_number_in_column(number,column) and not is_number_in_box(number,row,column)
 
+def solve():
+    for row in range(9):
+        for column in range(9):
+            if board[row][column] == 0:
+                for number_to_try in range(1,10):
+                    if is_valid_place(number_to_try,row,column):
+                        board[row][column] = number_to_try
+                        if solve():
+                            return True
+                        else:
+                            board[row][column] = 0
+                return False
+    return True
 
-
+print_board()
+solve()
+print("///////////")
 print_board()
